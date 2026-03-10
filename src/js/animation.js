7a526c6fe07026db970b13778284a0f405df0011
Simon N. <Pixelverk@gmail.com>
2026-03-10 15:05:19 +0100

    demo 1 klar

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,4 +1,16 @@
 /* animation demo 1 */
+const btn1 = document.getElementById("demo1-run");
+const container1 = document.getElementById("demo1-container");
+const img1 = document.getElementById("demo1-cat");
+let demo1_rotation = 0;
+
+btn1.addEventListener("click", () => {
+
+  demo1_rotation -= 50;
+
+  img1.style.transform = "rotate(" + demo1_rotation + "deg)";
+
+});
 
 /* animation demo 2 */
 const btn2 = document.getElementById("demo2-run");
